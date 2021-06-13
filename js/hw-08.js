1b72ca1ba24d4bceb3694d15dfe0874bc4f203cc
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-06-13 04:18:34 +0300

    clean code without the last additional task

--- js/hw-08.js
+++ js/hw-08.js
@@ -1,8 +1,9 @@
-import items from './apps.js';
+import items from '/js/apps.js';
 
 const galleryEl = document.querySelector(".js-gallery");
 const modalEl = document.querySelector(".js-lightbox");
 const buttonCloseEl = document.querySelector(".lightbox__button");
+const overlayEl = document.querySelector(".lightbox__overlay");
 
 const newItems = items
 .map(({original, preview, description}) => `<li class="gallery__item">
@@ -22,24 +23,45 @@ function onModalOpen (evt){
     };
 
     evt.preventDefault();
-    modalEl.classList.add("is-open");
 
-    
+    modalEl.classList.add("is-open");
 
     const imageModalEl = document.querySelector(".lightbox__image");
     
-    console.log(imageModalEl.src = evt.target.dataset.source);
-    console.log(imageModalEl.alt = evt.target.alt);
-
-
-
+    imageModalEl.src = evt.target.dataset.source;
+    imageModalEl.alt = evt.target.alt;
 
     buttonCloseEl.addEventListener("click", onModalClose);
 
+    overlayEl.addEventListener("click", onModalClose);
+
+    window.addEventListener("keydown", evt => {
+        if (evt.code === "Escape") {
+            onModalClose();
+            return;
+        };
+        // if (evt.code === "") {
+
+        //     return;
+        // };
+        // if (evt.code === "") {
+
+        // }
+    });
+
+
+
+    
 };
 
 function onModalClose (evt) {
-
     modalEl.classList.remove("is-open");
     buttonCloseEl.removeEventListener("click", onModalClose);
+    overlayEl.removeEventListener("click", onModalClose);
+    window.removeEventListener("keydown", evt => {
+        if (evt.code === "Escape") {
+            onModalClose();
+        };
+    });
 };
+
